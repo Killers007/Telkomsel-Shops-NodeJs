9091535a0c61c2c1c4d976ce56c3a3ff3731d0ca
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2022-11-24 15:49:18 +0800

    Test

--- anti_recaptcha_test.js
+++ anti_recaptcha_test.js
@@ -150,10 +150,7 @@ const { body, validationResult } = require("express-validator");
     }
   );
 
-  var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port);
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 })();
